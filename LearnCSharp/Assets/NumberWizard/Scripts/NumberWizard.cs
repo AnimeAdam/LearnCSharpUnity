018fb278a2e159a8edfa40c07cd58f666c1e7c70
AnimeAdam <Adam.Szlamp@gmail.com>
2022-02-16 20:16:58 +0000

    Finished Number Wizard

---
+++ LearnCSharp/Assets/NumberWizard/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] int guess;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartGame();
+	}
+	
+    void StartGame()
+    {
+        NextGuess();
+        max = max + 1;
+    }
+
+    public void NextGuess()
+    {
+        guess = Random.Range(min, max+1);
+        guessText.text = guess.ToString();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+}
