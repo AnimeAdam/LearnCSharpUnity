90af46454205200f759d644a58e1dfc64050f1c5
AnimeAdam <Adam.Szlamp@gmail.com>
2022-04-08 02:27:16 +0100

    Pure C# Observer Pattern

--- LearnCSharp/Assets/DesignPatterns/ObserverPattern/Patterns/Observer/Health.cs
+++ LearnCSharp/Assets/DesignPatterns/ObserverPattern/Patterns/Observer/Health.cs
@@ -2,14 +2,31 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class Health : MonoBehaviour {
+public class Health : MonoBehaviour
+{
     [SerializeField] float fullHealth = 100f;
     [SerializeField] float drainPerSecond = 2f;
     float currentHealth = 0;
 
-    private void Awake() {
+    private void Awake()
+    {
         ResetHealth();
         StartCoroutine(HealthDrain());
+    }
+
+    private void Start()
+    {
+    }
+
+    private void OnEnable()
+    {
+        GetComponent<Level>().onLevelUp.AddListener(ResetHealth); //Sets a callback function whenever onLevelUp is invoked.
+        GetComponent<Level>().OnLevelUpAction += ResetHealth; //Adds to the events in OnLevelUpAction.
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<Level>().OnLevelUpAction -= ResetHealth;
     }
 
     public float GetHealth()
--- LearnCSharp/Assets/DesignPatterns/ObserverPattern/Patterns/Observer/Level.cs
+++ LearnCSharp/Assets/DesignPatterns/ObserverPattern/Patterns/Observer/Level.cs
@@ -6,8 +6,18 @@ public class Level : MonoBehaviour
 {
 
     [SerializeField] int pointsPerLevel = 200;
-    [SerializeField] UnityEvent onLevelUp; //Core of Observer
+    public UnityEvent onLevelUp; //Core of Observer
     int experiencePoints = 0;
+
+    //event = function pointer - special kind of multicast delegate that can only be invoked from within the class or struct where they are declared (the publisher class).
+    //Action = it's like Delegate, BUT is used for calling void functions.
+    public event System.Action OnLevelUpAction;
+
+    public delegate void CallbackType(); //Create function pointer type. COULD ALSO MAKE IT function(int i)
+    public event System.Action<float> OnLevelUpActionFloat; //like above function (int i)
+    public event CallbackType OnLevelUpActionDelegate; //Create instant to assign function to later on.
+
+
 
     IEnumerator Start()
     {
@@ -25,6 +35,8 @@ public class Level : MonoBehaviour
         if (GetLevel() > currentLevel)
         {
             onLevelUp.Invoke();
+            if (OnLevelUpAction != null)
+                OnLevelUpAction();
         }
     }
 
